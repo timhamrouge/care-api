96f6bad33b539606730ee62927c92536718bc9c6
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-06-08 22:42:52 +0100

    eventicon component

--- frontend/src/Pages/ObservationsPage.tsx
+++ frontend/src/Pages/ObservationsPage.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from "styled-components";
 
+import ObservationIcon from '../components/EventIcon';
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -18,10 +20,48 @@ const TimelineContainer = styled.ul`
   border-radius: 10px;
   padding: 16px;
   max-width: 600px;
+  list-style-type: none;
   @media (max-width: 800px) {
     width: 90%;
     margin-top: 36px;
   }
+`;
+
+const ObservationEventContainer = styled.li`
+  padding-bottom: 1.5rem;
+  border-left: 1px solid #67C8E0;
+  position: relative;
+  padding-left: 20px;
+  margin-left: 10px;
+  &:last-child{
+    border: 0px;
+    padding-bottom: 0;
+  }
+  &:before{
+    content: '';
+    width: 15px;
+    height: 15px;
+    background: white;
+    border: 1px solid #ADE1EE;
+    box-shadow: 1px 1px 0px #67C8E0;
+    box-shadow: 1px 1px 0px #67C8E0;
+    border-radius: 50%;
+    position: absolute;
+    left: -9px;
+    top: 0px;
+  }
+  }
+`;
+
+const ObservationEventHeader = styled.div`
+  display: flex;
+`;
+
+const ObservationEventBody = styled.div`
+  display: flex;
+  border: 4px solid #F2E8FA;
+  border-radius: 10px;
+  padding: 8px;
 `;
 
 const ObservationsPage = () => {
@@ -29,6 +69,8 @@ const ObservationsPage = () => {
   const [loading, setLoading] = useState(true);
   const [careRecipient, setCareRecipient] = useState<{date: { id: string, name: string}} | null>(null);
   const [observationEvents, setObservationEvents] = useState<{date: { id: string, name: string}} | null>(null);
+  // do something with pagination
+  const [pageNumber, setPageNumber] = useState<number>(1);
   const [pages, setPages] = useState<number | null>(null);
   const [observationEventsTotal, setObservationEventsTotal] = useState<number | null>(null);
 
@@ -38,7 +80,6 @@ const ObservationsPage = () => {
       try {
         const response = await fetch(`${process.env.REACT_APP_API_URL}/care-recipients/${care_recipient_id}`);
         const json = await response.json();
-        console.log(json)
         setCareRecipient(json.data);
       } catch (error) {
         console.error("Error fetching care recipient:", error);
@@ -65,20 +106,27 @@ const ObservationsPage = () => {
     fetchCareRecipient();
   }, [care_recipient_id]);
 
-  console.log(observationEvents, careRecipient)
-
   return (
   <Container>
     <TimelineContainer>
-      {!loading && observationEvents && observationEvents.map((observationEvent) => {
-        return (<>
-          hello timo
-        </>)
-})}
-      {/* <TimelineList>
-      
+      {/* fix types and props */}
+      {!loading && observationEvents && observationEvents.map((observationEvent: any) => {
+        return (<ObservationEventContainer key={observationEvent.id}>
+          {console.log(observationEvent)}
+          <ObservationEventHeader>
+          {observationEvent.timestamp}
 
-      </TimelineList> */}
+          </ObservationEventHeader>
+          <ObservationEventBody>
+            <ObservationIcon observationType={observationEvent.event_type}/>
+            {observationEvent.event_type} by {observationEvent.Caregiver ? `${observationEvent.Caregiver.first_name} ${observationEvent.Caregiver.last_name}` : 'a carer'}
+          
+            
+          </ObservationEventBody>
+
+
+        </ObservationEventContainer>)
+      })}
     </TimelineContainer>
 
   </Container>
---
+++ frontend/src/components/EventIcon.tsx
@@ -0,0 +1,41 @@
+// import { Root } from "./styles";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBrain, faStethoscope, faGlassWater, faDroplet, faFaceSmile, faSyringe, faEye, faUtensils } from '@fortawesome/free-solid-svg-icons'
+import styled from "styled-components";
+
+
+interface Props {
+  observationType: string;
+}
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  margin-right: 8px;
+`
+
+const ObservationIcon = ({observationType}: Props) => {
+  const getIcon = (eventType: any) => {
+    switch (eventType) {
+      case 'fluid_intake_observation':
+        return <StyledIcon icon={faGlassWater} />
+      case 'physical_health_observation':
+        return <StyledIcon icon={faStethoscope} />
+      case 'mental_health_observation':
+        return <StyledIcon icon={faBrain} />
+      case 'food_intake_observation':
+        return <StyledIcon icon={faUtensils} />
+      case 'incontinence_pad_observation':
+        return <StyledIcon icon={faDroplet} />
+      case 'catheter_observation':
+        return <StyledIcon icon={faSyringe} />
+      case 'mood_observation':
+        return <StyledIcon icon={faFaceSmile} />
+      default:
+        return <StyledIcon icon={faEye} />
+    }
+  }
+  return (
+    getIcon(observationType)
+  )
+}
+
+export default ObservationIcon;
