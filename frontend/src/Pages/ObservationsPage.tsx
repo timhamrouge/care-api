20336e51ef09c7030bac2bb98e7843b2ec18ceb1
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-06-08 21:16:57 +0100

    start observation page

--- frontend/src/Pages/ObservationsPage.tsx
+++ frontend/src/Pages/ObservationsPage.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TimelineContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+  width: 60%;
+  border: 4px solid #ADE1EE;
+  border-radius: 10px;
+  padding: 16px;
+  max-width: 600px;
+  @media (max-width: 800px) {
+    width: 90%;
+    margin-top: 36px;
+  }
+`;
+
 const ObservationsPage = () => {
   return (
-  <>
-    hello timothy
-  </>
+  <Container>
+    <TimelineContainer>
+
+    </TimelineContainer>
+
+  </Container>
   )
 }
 
