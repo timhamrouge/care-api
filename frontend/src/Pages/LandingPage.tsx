7abb4c46bf8f8a2a0f966fd0c5ba5b02c9c34ca3
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-06-08 20:38:09 +0100

    start making the landing page lovely

--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Heading = styled.h2`
+  margin-top: 0;
+`;
+
 const LandingPage = () => {
   return (
-    <>
-      hello tim
-    </>
+    <Container>
+      <div>
+        <Heading>Welcome to the birde care records portal!</Heading>
+
+        <p>Stay connected with your elderly relatives by accessing their care records in one convenient place. Gain insights into their well-being and stay informed about the care they receive.</p>
+
+        <p>To get started, simply select the family member you would like to view the care records for below. We understand the importance of family bonds, and we're here to provide you with a transparent and compassionate way to stay involved in their care journey.</p>
+      </div>
+    </Container>
   )
 }
 
