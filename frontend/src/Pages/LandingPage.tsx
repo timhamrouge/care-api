ecf3205124be9935be04d20650d45e50ab3406fa
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-06-08 20:44:34 +0100

    nice styling

--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -8,6 +8,20 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+  width: 60%;
+  border: 4px solid #ADE1EE;
+  border-radius: 10px;
+  padding: 16px;
+  @media (max-width: 800px) {
+    width: 90%;
+    margin-top: 36px;
+  }
+`;
+
 const Heading = styled.h2`
   margin-top: 0;
 `;
@@ -15,13 +29,13 @@ const Heading = styled.h2`
 const LandingPage = () => {
   return (
     <Container>
-      <div>
-        <Heading>Welcome to the birde care records portal!</Heading>
+      <ContentBox>
+        <Heading>Welcome to the birde care observations portal!</Heading>
 
-        <p>Stay connected with your elderly relatives by accessing their care records in one convenient place. Gain insights into their well-being and stay informed about the care they receive.</p>
+        <p>Stay connected with your elderly relatives by accessing their care records in one convenient place. Gain insights into their well-being and stay informed about the care they receive by viewing the observations our carers are making in real time.</p>
 
-        <p>To get started, simply select the family member you would like to view the care records for below. We understand the importance of family bonds, and we're here to provide you with a transparent and compassionate way to stay involved in their care journey.</p>
-      </div>
+        <p>To get started, simply select the family member you would like to view the observations for below. We understand the importance of family bonds, and we're here to provide you with a transparent and compassionate way to stay involved in their care journey.</p>
+      </ContentBox>
     </Container>
   )
 }
