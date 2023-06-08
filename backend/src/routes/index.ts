0c3265a3fadf0104c60216d6d79c5e30fb71eeee
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-06-08 17:05:03 +0100

    add the new route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,18 @@ import express from "express";
 const router = express.Router();
 
 import careRecipientsController from "../controllers/careRecipientsController";
+import eventsController from "../controllers/eventsController";
 
 router.get("/care-recipients", careRecipientsController.findAll);
 
 router.get(
   "/care-recipients/:care_recipient_id",
   careRecipientsController.findOneById
+);
+
+router.get(
+  "/events/:care_recipient_id",
+  eventsController.getAllObservationEventsByCareRecipientId
 );
 
 export default router;
