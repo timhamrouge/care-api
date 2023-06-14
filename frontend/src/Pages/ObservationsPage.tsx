b5a062737a1da340f8585fb70e0ad5d0fe95451f
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-06-14 21:47:57 +0100

    implement pagination and a select

--- frontend/src/Pages/ObservationsPage.tsx
+++ frontend/src/Pages/ObservationsPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from "styled-components";
+import Select from 'react-select';
 import Observation from '../components/Observation';
 
 const Container = styled.div`
@@ -11,6 +12,21 @@ const Container = styled.div`
 `;
 
 const TimelineContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  border: 4px solid #ADE1EE;
+  border-radius: 10px;
+  padding: 16px;
+  max-width: 600px;
+  list-style-type: none;
+  @media (max-width: 800px) {
+    width: 90%;
+    margin-top: 36px;
+  }
+`;
+
+const FiltersContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 100px;
@@ -25,6 +41,41 @@ const TimelineContainer = styled.ul`
     margin-top: 36px;
   }
 `;
+
+const Pagination = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  padding-bottom: 16px;
+`
+
+const PaginationButton = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #38827e;
+  color: #ffffff;
+  width: 72px;
+  padding: 8px;
+  border-radius: 10px;
+  border: 0px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #49ABA6;
+  }
+
+  &:active {
+    transform: translateY(1px);
+    background-color: #26615f;
+  }
+`
+
+const PaginationButtons = styled.div`
+  display: flex;
+  gap: 8px;
+`
 
 const ObservationsPage = () => {
   const { care_recipient_id } = useParams();
@@ -51,7 +102,7 @@ const ObservationsPage = () => {
     if (care_recipient_id) {
       const fetchObservationEvents = async () => {
         try {
-          const response = await fetch(`${process.env.REACT_APP_API_URL}/observations/${care_recipient_id}`);
+          const response = await fetch(`${process.env.REACT_APP_API_URL}/observations/${care_recipient_id}?page=${pageNumber}`);
           const json = await response.json();
           setPages(json.pages);
           setObservationEventsTotal(json.total);
@@ -66,16 +117,60 @@ const ObservationsPage = () => {
 
     setLoading(false);
     fetchCareRecipient();
-  }, [care_recipient_id]);
+  }, [care_recipient_id, pageNumber]);
+
+  const selectOptions = [
+    { label: "Fluid intake", value: "fluid_intake" },
+    { label: "Physical health observation", value: "physical_health_observation" },
+    { label: "Mood", value: "mood" },
+    { label: "Incontinence pad observation", value: "incontinence_pad_observation" },
+    { label: "General", value: "general" },
+    { label: "Food intake", value: "food_intake" },
+    { label: "Mental health", value: "mental_health" },
+    { label: "Catheter observation", value: "catheter_observation" }
+  ];
+
+  console.log(pageNumber, pages)
+
+  const changePage = (event) => {
+    const buttonText = event.target.outerText;
+    if (buttonText === "Next") {
+      setPageNumber(pageNumber + 1)
+    }
+
+    if (buttonText === "Back") {
+      setPageNumber(pageNumber - 1)
+    }
+  }
 
   return (
   <Container>
+    <FiltersContainer>
+      <Select
+        // defaultValue={[colourOptions[2], colourOptions[3]]}
+        isMulti
+        name="event-type-filters"
+        options={selectOptions}
+      />
+    </FiltersContainer>
     <TimelineContainer>
       {/* fix types and props */}
       {!loading && observationEvents && observationEvents.map((observationEvent: any) => {
         return (<Observation observation={observationEvent} key={observationEvent.id}/>)})}
     </TimelineContainer>
 
+    {!loading && observationEvents && 
+      <Pagination>
+        Page {pageNumber} of {pages}
+        <PaginationButtons>
+          {pageNumber !== 1 && 
+            <PaginationButton onClick={changePage}>Back</PaginationButton>
+          }
+          {pageNumber !== pages && 
+            <PaginationButton onClick={changePage}>Next</PaginationButton>
+          }
+        </PaginationButtons>
+    </Pagination>}
   </Container>
   )
 }
